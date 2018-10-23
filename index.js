b3f96371d84444fde9aba55ed7a5e0903c837662
SVPA-CharlieSantos <charlemagne.santos@am.sony.com>
2018-10-23 09:29:49 -0700

    calling callback on done

--- index.js
+++ index.js
@@ -87,11 +87,9 @@ function getIt(options, done) {
         playlistsFinished++;
         console.log('playlists finished: ', playlistsFinished);
         if (mediaPlaylist) {
-          console.log('shutting down');
-          process.exit();
+          done && done();
         } else if (playlistsFinished == masterPlaylist.mediaPlaylists.length) {
-          console.log('shutting down');
-          process.exit();
+          done && done();
         }
         }, finishedDownloadingSegment);
 
@@ -121,7 +119,9 @@ function getIt(options, done) {
         rootUri = path.dirname(pl[i].uri);
         updateFunction = pl[i].update.bind(pl[i]);
         downloadFunction = pl[i].download.bind(pl[i]);
-        downloadFunction(rootUri, cwd, pl[i].bandwidth, function() {console.log('shutting down');process.exit();}, finishedDownloadingSegment);
+        downloadFunction(rootUri, cwd, pl[i].bandwidth, function() {
+          done && done();
+        }, finishedDownloadingSegment);
         if (!pl[i].endList) {
           //Only set update if we haven't found an endlist
           updateInterval = setInterval(updateFunction, pl[i].targetDuration * 1000, rootUri);
